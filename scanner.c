27420fb721bdd9006111cb22785e021477a74823
Thomas1198 <tom.dorda@gmail.com>
2018-10-16 10:17:14 +0200

    added scanner.c .h

---
+++ scanner.c
@@ -0,0 +1,5 @@
+//
+// Created by tomdo on 16.10.2018.
+//
+
+#include "scanner.h"
---
+++ scanner.h
@@ -0,0 +1,8 @@
+//
+// Created by tomdo on 16.10.2018.
+//
+
+#ifndef IFJ_SCANNER_H
+#define IFJ_SCANNER_H
+
+#endif //IFJ_SCANNER_H
