2b33a45c4283708cb9b5eb627281237c80eede26
kony <kk.kony@seznam.cz>
2018-11-17 15:42:18 +0100

    Merge branch 'master' of https://gitlab.com/ifj-projekt-group/ifj
    # Conflicts:
    #	CMakeLists.txt

--- parser.c
+++ parser.c
@@ -2,7 +2,7 @@
 
 void run_parser(FILE *source_code) {
     struct tToken token_actual;
-    tToken_list token_list;
+    tDList token_list;
 
     while ((token_actual = get_token(source_code)).content_string != NULL) {
         if (add_to_token_list(&token_list, token_actual)) {
