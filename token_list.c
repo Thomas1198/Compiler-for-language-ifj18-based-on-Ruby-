47922eab28ab60140d7e52033d06325dfdf5ed96
Thomas1198 <33345883+Thomas1198@users.noreply.github.com>
2018-11-17 15:22:36 +0100

    token list

--- token_list.c
+++ token_list.c
@@ -5,3 +5,212 @@
  */
 
 #include "token_list.h"
+
+void DLInitList (tDLList *L) {
+    L->First = NULL;
+    L->Act = NULL;
+    L->Last = NULL;
+}
+
+void DLDisposeList (tDLList *L) {
+    while (L->First != NULL) {
+        tDLElemPtr tmp = L->First;
+        L->First = L->First->rptr;  //presmerovani zacatku na prvek napravo
+        free(tmp);
+    }
+
+    L->Act = NULL;
+    L->Last = NULL;
+}
+
+void DLInsertFirst (tDLList *L, int val) {
+    tDLElemPtr newElement = malloc(sizeof(struct tDLElem));
+    if (newElement == NULL)
+        DLError();
+    else {
+        newElement->data = val;
+        newElement->lptr = NULL;
+        newElement->rptr = L->First;
+
+        if (L->First == NULL)
+            L->Last = newElement; //jednina polozka => je zaroven i konec
+        else
+            L->First->lptr = newElement;
+
+        L->First = newElement;
+    }
+}
+
+void DLInsertLast(tDLList *L, int val) {
+    tDLElemPtr newElement = malloc(sizeof(struct tDLElem));
+    if (newElement == NULL)
+        DLError();
+    else {
+        newElement->data = val;
+        newElement->lptr = L->Last;
+        newElement->rptr = NULL;
+
+        if (L->First == NULL)
+            L->First = newElement;  //jednina polozka => je zaroven i zacatek
+        else
+            L->Last->rptr = newElement;
+
+        L->Last = newElement;
+    }
+}
+
+void DLFirst (tDLList *L) {
+    L->Act = L->First;
+}
+
+void DLLast (tDLList *L) {
+    L->Act = L->Last;
+}
+
+void DLCopyFirst (tDLList *L, int *val) {
+    if (L->First != NULL)
+        *val = L->First->data;
+    else
+        DLError();
+}
+
+void DLCopyLast (tDLList *L, int *val) {
+    if (L->First != NULL)
+        *val = L->Last->data;
+    else
+        DLError();
+}
+
+void DLDeleteFirst (tDLList *L) {
+
+    if(L->First != NULL) {
+        if (L->First == L->Act)
+            L->Act = NULL;
+
+        tDLElemPtr tmp = L->First;
+
+        if (L->First == L->Last) {
+            L->First = NULL;
+            L->Last = NULL;
+        }
+        else {
+            L->First = L->First->rptr;
+            L->First->lptr = NULL;
+        }
+
+        free(tmp);
+    }
+}
+
+void DLDeleteLast (tDLList *L) {
+    if(L->First != NULL) {
+        if (L->Last == L->Act)
+            L->Act = NULL;
+
+        tDLElemPtr tmp = L->Last;
+
+        if (L->First == L->Last) {
+            L->First = NULL;
+            L->Last = NULL;
+        }
+        else {
+            L->Last = L->Last->lptr;
+            L->Last->rptr = NULL;
+        }
+
+        free(tmp);
+    }
+}
+
+void DLPostDelete (tDLList *L) {
+    if (L->First != NULL && L->Act != NULL && L->Act != L->Last) {
+        tDLElemPtr tmp = L->Act->rptr;
+        L->Act->rptr = tmp->rptr;
+        if (tmp == L->Last)
+            L->Last = L->Act;
+        else
+            tmp->rptr->lptr = L->Act;
+
+        free(tmp);
+    }
+}
+
+void DLPreDelete (tDLList *L) {
+    if (L->First != NULL && L->Act != NULL && L->Act != L->First) {
+        tDLElemPtr tmp = L->Act->lptr;
+        L->Act->lptr = tmp->lptr;
+        if (tmp == L->First)
+            L->First = L->Act;
+        else
+            tmp->lptr->rptr = L->Act;
+
+        free(tmp);
+    }
+}
+
+void DLPostInsert (tDLList *L, int val) {
+    if (L->Act != NULL) {
+        tDLElemPtr newElement = malloc(sizeof(struct tDLElem));
+        if (newElement == NULL)
+            DLError();
+        else {
+            newElement->data = val;
+            newElement->lptr = L->Act;
+            newElement->rptr = L->Act->rptr;
+
+            L->Act->rptr = newElement;
+
+            if (L->Act == L->Last)
+                L->Last = newElement;
+            else
+                newElement->rptr->lptr = newElement;
+        }
+    }
+}
+
+void DLPreInsert (tDLList *L, int val) {
+    if (L->Act != NULL) {
+        tDLElemPtr newElement = malloc(sizeof(struct tDLElem));
+        if (newElement == NULL)
+            DLError();
+        else {
+            newElement->data = val;
+            newElement->lptr = L->Act->lptr;
+            newElement->rptr = L->Act;
+
+            L->Act->lptr = newElement;
+
+            if (L->Act == L->First)
+                L->First = newElement;
+            else
+                newElement->lptr->rptr = newElement;
+        }
+    }
+}
+
+void DLCopy (tDLList *L, int *val) {
+    if (L->Act != NULL)
+        *val = L->Act->data;
+    else
+        DLError();
+}
+
+void DLActualize (tDLList *L, int val) {
+    if (L->Act != NULL)
+        L->Act->data = val;
+}
+
+void DLSucc (tDLList *L) {
+    if (L->Act != NULL)
+        L->Act = L->Act->rptr;
+}
+
+
+void DLPred (tDLList *L) {
+    if (L->Act != NULL)
+        L->Act = L->Act->lptr;
+}
+
+int DLActive (tDLList *L) {
+    return L->Act != NULL;
+}
--- token_list.h
+++ token_list.h
@@ -16,4 +16,134 @@ typedef struct {
     tDLElemPtr First;
     tDLElemPtr Act;
     tDLElemPtr Last;
-} tDLList;
+} tDList;
+
+/*
+** Provede inicializaci seznamu L před jeho prvním použitím (tzn. žádná
+** z následujících funkcí nebude volána nad neinicializovaným seznamem).
+** Tato inicializace se nikdy nebude provádět nad již inicializovaným
+** seznamem, a proto tuto možnost neošetřujte. Vždy předpokládejte,
+** že neinicializované proměnné mají nedefinovanou hodnotu.
+**/
+void DLInitList (tDLList *L);
+
+/*
+** Zruší všechny prvky seznamu L a uvede seznam do stavu, v jakém
+** se nacházel po inicializaci. Rušené prvky seznamu budou korektně
+** uvolněny voláním operace free.
+**/
+void DLDisposeList (tDLList *L);
+
+/*
+** Vloží nový prvek na začátek seznamu L.
+** V případě, že není dostatek paměti pro nový prvek při operaci malloc,
+** volá funkci DLError().
+**/
+void DLInsertFirst (tDLList *L, int val);
+
+/*
+** Vloží nový prvek na konec seznamu L (symetrická operace k DLInsertFirst).
+** V případě, že není dostatek paměti pro nový prvek při operaci malloc,
+** volá funkci DLError().
+**/
+void DLInsertLast(tDLList *L, int val);
+
+/*
+** Nastaví aktivitu na první prvek seznamu L.
+** Funkci implementujte jako jediný příkaz (nepočítáme-li return),
+** aniž byste testovali, zda je seznam L prázdný.
+**/
+void DLFirst (tDLList *L);
+
+/*
+** Nastaví aktivitu na poslední prvek seznamu L.
+** Funkci implementujte jako jediný příkaz (nepočítáme-li return),
+** aniž byste testovali, zda je seznam L prázdný.
+**/
+void DLLast (tDLList *L)
+
+/*
+** Prostřednictvím parametru val vrátí hodnotu prvního prvku seznamu L.
+** Pokud je seznam L prázdný, volá funkci DLError().
+**/
+void DLCopyFirst (tDLList *L, int *val);
+
+/*
+** Prostřednictvím parametru val vrátí hodnotu posledního prvku seznamu L.
+** Pokud je seznam L prázdný, volá funkci DLError().
+**/
+void DLCopyLast (tDLList *L, int *val);
+
+/*
+** Zruší první prvek seznamu L. Pokud byl první prvek aktivní, aktivita
+** se ztrácí. Pokud byl seznam L prázdný, nic se neděje.
+**/
+void DLDeleteFirst (tDLList *L);
+
+/*
+** Zruší poslední prvek seznamu L. Pokud byl poslední prvek aktivní,
+** aktivita seznamu se ztrácí. Pokud byl seznam L prázdný, nic se neděje.
+**/
+void DLDeleteLast (tDLList *L);
+
+/*
+** Zruší prvek seznamu L za aktivním prvkem.
+** Pokud je seznam L neaktivní nebo pokud je aktivní prvek
+** posledním prvkem seznamu, nic se neděje.
+**/
+void DLPostDelete (tDLList *L);
+
+/*
+** Zruší prvek před aktivním prvkem seznamu L .
+** Pokud je seznam L neaktivní nebo pokud je aktivní prvek
+** prvním prvkem seznamu, nic se neděje.
+**/
+void DLPreDelete (tDLList *L);
+
+/*
+** Vloží prvek za aktivní prvek seznamu L.
+** Pokud nebyl seznam L aktivní, nic se neděje.
+** V případě, že není dostatek paměti pro nový prvek při operaci malloc,
+** volá funkci DLError().
+**/
+void DLPostInsert (tDLList *L, int val);
+
+/*
+** Vloží prvek před aktivní prvek seznamu L.
+** Pokud nebyl seznam L aktivní, nic se neděje.
+** V případě, že není dostatek paměti pro nový prvek při operaci malloc,
+** volá funkci DLError().
+**/
+void DLPreInsert (tDLList *L, int val);
+
+/*
+** Prostřednictvím parametru val vrátí hodnotu aktivního prvku seznamu L.
+** Pokud seznam L není aktivní, volá funkci DLError ().
+**/
+void DLCopy (tDLList *L, int *val);
+
+/*
+** Přepíše obsah aktivního prvku seznamu L.
+** Pokud seznam L není aktivní, nedělá nic.
+**/
+void DLActualize (tDLList *L, int val);
+
+/*
+** Posune aktivitu na následující prvek seznamu L.
+** Není-li seznam aktivní, nedělá nic.
+** Všimněte si, že při aktivitě na posledním prvku se seznam stane neaktivním.
+**/
+void DLSucc (tDLList *L);
+
+/*
+** Posune aktivitu na předchozí prvek seznamu L.
+** Není-li seznam aktivní, nedělá nic.
+** Všimněte si, že při aktivitě na prvním prvku se seznam stane neaktivním.
+**/
+void DLPred (tDLList *L);
+
+/*
+** Je-li seznam L aktivní, vrací nenulovou hodnotu, jinak vrací 0.
+** Funkci je vhodné implementovat jedním příkazem return.
+**/
+int DLActive (tDLList *L);
