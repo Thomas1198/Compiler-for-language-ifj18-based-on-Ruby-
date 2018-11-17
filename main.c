d62aa65447c32f4c9e1d740c603c26cbb426bf18
kony <kk.kony@seznam.cz>
2018-11-17 13:54:37 +0100

    Cmake

--- main.c
+++ main.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
+#include "error.h"
+#include "parser.h"
 
 int main(int argc, char* argv[]) {
 
+    FILE *source_code;
 
+    if(argc==1){
+        source_code=stdin;
+    } else if(argc==2){
+        if((source_code= fopen(argv[1],"r"))==NULL){
+            return INTERNAL_ERROR;
+        }
+    } else{
+        return INTERNAL_ERROR;
+    }
+
+
+    fclose(source_code);
 
     return 0;
 }
---
+++ parser.h
@@ -0,0 +1,8 @@
+//
+// Created by kony on 17.11.18.
+//
+
+#ifndef IFJ_PARSER_H
+#define IFJ_PARSER_H
+
+#endif //IFJ_PARSER_H
