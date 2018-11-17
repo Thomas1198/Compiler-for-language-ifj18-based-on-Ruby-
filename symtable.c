690fbfbc58e6f480d7198f8a641f5f974a3c94f4
Thomas1198 <33345883+Thomas1198@users.noreply.github.com>
2018-11-17 19:23:56 +0100

    working on symtable

--- symtable.c
+++ symtable.c
@@ -5,3 +5,62 @@
  */
 
 #include "symtable.h"
+
+/**
+ * @brief calculates hash and returning index to table
+ * @param str string which is used to calculate hash
+ * @return table index.
+ */
+static unsigned long hash(const char *str)
+{
+    unsigned int h = 0;
+    const unsigned char *p;
+    for(p = (const unsigned char*) str; *p != '\0'; p++)
+        h = 65599*h + *p;
+
+    return h % MAX_TABLE_SIZE;
+}
+
+void symtable_create(Symtable *table) {
+    if (table != NULL)
+        for (int i = 0; i < MAX_TABLE_SIZE; i++)
+            (*table)[i] = NULL;
+}
+
+struct tToken *symtable_insert(Symtable *table, const char *key){
+
+}
+
+struct tToken *symtable_get(Symtable *table, const char *key){
+
+}
+
+bool symtable_remove(Symtable *table, const char *key){
+
+}
+
+void symtable_destroy(Symtable *table){
+    if (table == NULL)
+        return;
+
+    Titem *tmp_next = NULL;
+
+    for (int i = 0; i < MAX_TABLE_SIZE; i++)
+    {
+        for (Titem *tmp = (*table)[i]; tmp != NULL; tmp = tmp_next)
+        {
+            tmp_next = tmp->next;
+            free(tmp->key);
+
+            if (tmp->data.params != NULL)
+            {
+                dynamicstring_free(tmp->data.params);
+                free(tmp->data.params);
+            }
+
+            free(tmp);
+        }
+
+        (*table)[i] = NULL;
+    }
+}
--- symtable.h
+++ symtable.h
@@ -10,11 +10,60 @@
 #include "dynamic_string.h"
 #include "token.h"
 
+#define MAX_TABLE_SIZE 27457 //TODO change the number
+
 /**
  * @struct Symbol table item representation
  */
-typedef struct htab_listitem {
+typedef struct htab_item {
     Dynamic_string *key;     /// identifier
     struct tToken data;      /// data
-    struct htab_listitem *next; /// pointer to next item
+    struct htab_item *next; /// pointer to next item
 } Titem;
+
+
+// Symbol table
+typedef Titem* Symtable[MAX_TABLE_SIZE];
+
+
+/**
+ * @brief initialise symbol table
+ *
+ * @param tab pointer on table
+ */
+void symtable_create(Symtable *tab);
+
+/**
+ * @brief Appends item to table
+ *
+ * @param table pointer to table.
+ * @param key function identifier or variable
+ * @return NULL if error or item is existing else returns pointer to added item.
+ */
+struct tToken *symtable_insert(Symtable *table, const char *key);
+
+/**
+ * @brief finds symbol and returns its data
+ *
+ * @param table pointer to table
+ * @param key function identifier or variable
+ * @return NULL if not existing symbol, pointer to data of symbol if successfully found.
+ */
+struct tToken *symtable_get(Symtable *table, const char *key);
+
+
+/**
+ * @brief removes symbol
+ *
+ * @param table pointer to table
+ * @param key function identifier or variable
+ * @return true if successful, false if unsuccessful.
+ */
+bool symtable_remove(Symtable *table, const char *key);
+
+/**
+ * @brief frees resources
+ *
+ * @param table pointer to table
+ */
+void symtable_destroy(Symtable *table);
