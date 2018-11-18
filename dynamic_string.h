490a4e4d11efd2d8d382b0064d1953ae50e113cb
awariss <jena.osolso@gmail.com>
2018-11-18 13:40:13 +0100

    Dynamic string

--- dynamic_string.h
+++ dynamic_string.h
@@ -1,5 +1,72 @@
 /**
  * @file dynamic_string.h
- * @author Tomas Dorda (xdorda00)
- * @brief
+ * @author Jan Osolsobe (xosols00)
+ * @brief Dynamic string interface
  */
+
+#ifndef _DYNAMIC_STRING_H
+#define _DYNAMIC_STRING_H
+
+#include <stdbool.h>
+/**
+ * @struct Dynamic string
+ */
+typedef struct
+{
+    char *str;  //  string
+    unsigned int length;    // length of string
+    unsigned int alloc_size;    //  number of chars alocated for string
+} Dynamic_string;
+
+/**
+ * Inicialization dynamic string
+ * @param s Pointer to dynamic string
+ * @return true if its ok
+ */
+bool  dynamic_str_init(Dynamic_string *s);
+
+/**
+ * Frees alocated memory
+ * @param s Pointer to dynamic string
+ */
+void dynamic_str_free(Dynamic_string *s);
+
+/**
+ * Clean dynamic string
+ * @param s Pointer to dynamic string
+ */
+void dynamic_str_clear(Dynamic_string *s);
+
+/**
+ * Add char to the end of string
+ * @param s Pointer to dynamic string
+ * @param c char to add
+ * @return true if its ok
+ */
+bool dynamic_str_add_char(Dynamic_string *s, char c);
+
+/**
+ * Add string to the end of string
+ * @param s Pointer to dynamic string
+ * @param const_string String to add
+ * @return true if its ok
+ */
+bool dynamic_str_add_const_str(Dynamic_string *s, const char *const_string);
+
+/**
+ * Compare dynamic string with constante string
+ * @param dynamic_string dynamic string
+ * @param const_string constant string
+ * @return 1 if s1 is greater than s2, 0 if itd equal, -1 if its less
+ */
+int dynamic_str_cmp_const_str(Dynamic_string *dynamic_string, const char *const_string);
+
+/**
+ * Copy string to destination
+ * @param src string
+ * @param dest destination to copy
+ * @return True if its ok
+ */
+bool dynamic_str_copy(Dynamic_string *src, Dynamic_string *dest);
+
+#endif // _DYNAMIC_STRING_H
