7bb1d4643b96a7eb120174a5ae47cf0917cba46a
awariss <jena.osolso@gmail.com>
2018-11-20 12:20:22 +0100

    Pridano zacatky code generatoru

---
+++ code_generator.c
@@ -0,0 +1,12 @@
+/**
+ * @brief Code generator
+ * @file code_generator.h
+ * @author Jan Osolsobe (xosols00)
+ */
+
+
+#include <ctype.h>
+
+#include "dynamic_string.h"
+#include "scanner.h"
+#include "code_generator.h"
---
+++ code_generator.h
@@ -0,0 +1,19 @@
+/**
+ * @brief Code generator
+ * @file code_generator.h
+ * @author Jan Osolsobe (xosols00)
+ */
+
+#ifndef PROJECT_CODE_GENERATOR_H
+#define PROJECT_CODE_GENERATOR_H
+
+#include <stdio.h>
+#include <stdbool.h>
+
+#include "symtable.h"
+#include "scanner.h"
+
+
+
+
+#endif //PROJECT_CODE_GENERATOR_H
