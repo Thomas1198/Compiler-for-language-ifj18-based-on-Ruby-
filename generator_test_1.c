88bab4c9824dc0058fc5608af98b9abdddab85c9
awariss <jena.osolso@gmail.com>
2018-11-21 10:33:26 +0100

    test pro generator

---
+++ generator_test_1.c
@@ -0,0 +1,24 @@
+//
+// Created by Jan on 21.11.2018.
+//
+/**
+ * @brief Code generator test
+ * @file generator_test_1.c
+ * @author Jan Osolsobe (xosols00)
+ */
+
+
+#include <ctype.h>
+
+#include "code_generator.h"
+
+int main()
+{
+    generator_start();
+    generate_file_head();
+    generate_main_start();
+    generate_main_end();
+    write_code();
+    generator_clear();
+    return 0;
+};
