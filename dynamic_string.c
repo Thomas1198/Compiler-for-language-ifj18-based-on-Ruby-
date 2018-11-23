b7ece23009430a5b10a1bf48774c82ab4ee90845
Thomas1198 <33345883+Thomas1198@users.noreply.github.com>
2018-11-23 21:28:38 +0100

    functions in dynstr changed to void and calling ErrorPrint

--- dynamic_string.c
+++ dynamic_string.c
@@ -11,21 +11,20 @@ void dynamic_string_clear(Dynamic_string *s) {
     s->str[s->length] = '\0';
 }
 
-bool dynamic_string_init(Dynamic_string *s) {
+void dynamic_string_init(Dynamic_string *s) {
     if ((s->str = (char *) malloc(DYNAMIC_STRING_LENGTH))) {
         dynamic_string_clear(s);
         s->alloc_size = DYNAMIC_STRING_LENGTH;
-        return true;
     }
     else
-        return false;
+        ErrorPrint(INTERNAL_ERROR, "Internal fault in dynamic_string.c");
 }
 
 void dynamic_string_free(Dynamic_string *s) {
     free(s->str);
 }
 
-bool dynamic_string_add_char(Dynamic_string *s, char c) {
+bool dynamic_string_add_char(Dynamic_string *s, char c) {   //TODO: nastavit na VOID
     if ((s->length + 1) >= s->alloc_size) {
         unsigned int new_size = s->length + DYNAMIC_STRING_LENGTH;
         if ((s->str = (char *) realloc(s->str, new_size)))
@@ -38,19 +37,18 @@ bool dynamic_string_add_char(Dynamic_string *s, char c) {
     return true;
 }
 
-bool dynamic_string_add_const_str(Dynamic_string *s, const char *const_string) {
+void dynamic_string_add_const_str(Dynamic_string *s, const char *const_string) {
     unsigned int const_str_len = (unsigned int) strlen(const_string);
     if ((s->length + const_str_len + 1) >= s->alloc_size) {
         unsigned int new_size = s->length + const_str_len + 1;
         if ((s->str = (char *) realloc(s->str, new_size)))
             s->alloc_size = new_size;
         else
-            return false;
+            ErrorPrint(INTERNAL_ERROR, "Internal fault in dynamic_string.c");
     }
     s->length = s->length + const_str_len;
     strcat(s->str, const_string);
     s->str[s->length] = '\0';
-    return true;
 }
 
 int dynamic_string_cmp_const_str(Dynamic_string *dynamic_string, const char *const_string) {
@@ -58,15 +56,14 @@ int dynamic_string_cmp_const_str(Dynamic_string *dynamic_string, const char *con
     return result;
 }
 
-bool dynamic_string_copy(Dynamic_string *src, Dynamic_string *dest) {
+void dynamic_string_copy(Dynamic_string *src, Dynamic_string *dest) {
     unsigned int new_len = src->length;
     if (new_len >= dest->alloc_size) {
         if ((dest->str = (char *) realloc(dest->str, new_len)))
             dest->alloc_size = new_len + 1;
         else
-            return false;
+            ErrorPrint(INTERNAL_ERROR, "Internal fault in dynamic_string.c");
     }
     strcpy(dest->str, src->str);
     dest->length = new_len;
-    return true;
 }
