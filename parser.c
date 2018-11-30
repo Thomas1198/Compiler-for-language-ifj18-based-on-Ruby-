39fbf8224bb4be4c2540be0630a23c8f586816e5
kony <kk.kony@seznam.cz>
2018-11-30 14:50:09 +0100

    kony

--- parser.c
+++ parser.c
@@ -57,19 +57,18 @@ void first_run(tDList *token_list, FILE *source_code) {
                     function_par = true;
                 }
 
-                if (is_set_type(token_list->Last->lptr->token, KEY_WORD_IF) ||
-                    is_set_type(token_list->Last->lptr->token, KEY_WORD_WHILE)) {
-                    end_count++;
-                }
+            if (is_set_type(token_list->Last->lptr->token, KEY_WORD_IF) ||
+                is_set_type(token_list->Last->lptr->token, KEY_WORD_WHILE)) {
+                end_count++;
             }
+        }
         }
 
         if (is_set_type(token_actual, CHAR_EQUALS)) {
-            *tmp = NULL;
-            if (token_list->Last->lptr != NULL) {
+            if(token_list->Last->lptr != NULL){
                 *tmp = token_list->Last->lptr->token;
-            }
 
+            }
 
             if ((tmp = symtable_get(&hTable, tmp->content_string)) == NULL) {
                 symtable_insert(&hTable, tmp);
