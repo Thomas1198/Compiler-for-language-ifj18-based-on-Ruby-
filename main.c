bd8af3686bf3a912113fb86cc201d10d1fdbd764
Thomas1198 <33345883+Thomas1198@users.noreply.github.com>
2018-12-06 00:00:57 +0100

    final edits

--- main.c
+++ main.c
@@ -1,28 +1,30 @@
+/**
+ * @file main.c
+ * @author Tomas Dorda (xdorda00), Jakub Konetzny, Jan Osolsobe, Patrik Strnad
+ */
+
 #include <stdio.h>
 
 #include "parser.h"
 #include "error.h"
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
 
     FILE *source_code;
 
-    if(argc==1){
-        source_code=stdin;
-    } else if(argc==2){
-        if((source_code= fopen(argv[1],"r"))==NULL){
+    if (argc == 1) {
+        source_code = stdin;
+    } else if (argc == 2) {
+        if ((source_code = fopen(argv[1], "r")) == NULL) {
             return INTERNAL_ERROR;
         }
-    } else{
+    } else {
         return INTERNAL_ERROR;
     }
+
     run_parser(source_code);
 
-
-
     fclose(source_code);
-
-
 
     return 0;
 }
